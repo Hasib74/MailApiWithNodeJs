4f66070dddfbce659ec355402543af7aa2047fa6
hasib74 <hasibakon74@gmail.com>
2019-11-10 23:40:38 +0600

    make it better

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use((req,res,next)=>{
 
 
   if(req.method === 'OPTIONS'){
-      req.header('Access-Control-Allow-Methods','PUT');
+      req.header('Access-Control-Allow-Methods','POST');
       return res.status(200).json({});
   }
   next();
