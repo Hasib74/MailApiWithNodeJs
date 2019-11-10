cd57207c0f284b45ba3185bcfb05e58cf4068445
hasib74 <hasibakon74@gmail.com>
2019-11-10 20:25:27 +0600

    make it better

--- server.js
+++ server.js
@@ -66,7 +66,32 @@ app.post("/email",(req,res)=>{
              })
         }
 
-})  });
+     })  
+});
+
+app.use((req,res,next)=>{
+
+  const error=new Error('Not found');
+  error.status=400;
+
+  next(error);
+
+});
+
+
+app.use (  (error,req,res,next)=>{
+
+  res.status(error.status || 500);
+  res.json({
+      error:{
+         message:error.message
+
+      }
+  }) });
+
+
+
+
    
 
 app.listen(  process.env.PORT || '3000', () => {
